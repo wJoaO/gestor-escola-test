5a712616ca2d83d8ed17537c55958b0ad3ff9085
João Vitor <jvmdo2008@gmail.com>
2020-10-11 10:32:29 -0300

    Adicionado roteamento básico. Mudado configurações angular cli.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'escola/lista',
+    loadChildren: () => import('./pages/school-list/school-list.module').then(m => m.SchoolListPageModule)
+  },
+  {
+    path: 'escola/detalhe/:id',
+    loadChildren: () => import('./pages/school-detail/school-detail.module').then(m => m.SchoolDetailPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'escola/lista',
     pathMatch: 'full'
   },
 ];
